27b51585712b131bc374d7a4478e6a9521a45394
Muhammad Rehan <murehan@nisum.com>
2026-08-31 13:30:09 +0500

    fix class validator not validating nested dto in create custoer dto

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,6 +1,10 @@
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { CreateInternetLogonDto } from './create-internet-logon.dto';
+import { ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateCustomerDto extends CreateUserDto {
+  @ValidateNested()
+  @Type(() => CreateInternetLogonDto)
   internetLogonData!: CreateInternetLogonDto;
 }
